ce0a7e5aba9c5c2684160ddbed79d3e664053272
HarDhillon <66462387+HarDhillon@users.noreply.github.com>
2020-07-07 16:46:12 +0800

    Add files via upload

---
+++ day3.1.py
@@ -0,0 +1,22 @@
+file = open('day3.info.txt')
+(wire1steps, wire2steps) = file.read().split()
+
+wire1steps = wire1steps.split(',')
+# wire 1 = [ R8 , U5 , D4 , L7 ]
+
+wire2steps = wire2steps.split(',')
+
+origin = [0,0]
+
+wire1pos = [0,0]
+wire2pos = [0,0]
+
+# horizontal movement =  h
+# vertical movement = v
+
+for h,v in wire1pos:
+    if
+    print ('distance =', x[1:])
+
+# make a tuple pair for each pos on first wire ( wire1 ) and then
+#compare that to a tuple pair of each pos on the other wire ( wire2 )
